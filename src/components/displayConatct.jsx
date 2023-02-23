81b13e8a8c2cebf077a353bb44bd12e73bf06b25
saurabh.dagwar <saurabh.dagwar@magicsw.com>
2023-02-24 00:34:30 +0530

    started new project of contact list

---
+++ src/components/displayConatct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DisplayConatct = () => {
+    return (
+        <div className='display-conatct'>
+            
+        </div>
+    );
+};
+
+export default DisplayConatct;
---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DisplayConatct from './displayConatct';
+
+const Main = () => {
+    return (
+        <div className='main-component'>
+            <DisplayConatct />
+        </div>
+    );
+};
+
+export default Main;
